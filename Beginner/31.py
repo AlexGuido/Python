7e99d593764770a2559a7dcb5e3513ed4a7e998a
Alex Guido <34007499+AlexGuido@users.noreply.github.com>
2020-01-22 01:12:53 -0600

    Create 31.py

---
+++ Beginner/31.py
@@ -0,0 +1,10 @@
+'''
+Diseña un programa que, a partir del valor del lado de un cuadrado (3 metros), muestre
+el valor de su perímetro (en metros) y el de su área (en metros cuadrados).
+(El perímetro debe darte 12 metros y el área 9 metros cuadrados).
+33
+'''
+perimetro = 3*4 #perimetro del cuadrado formula 4*l
+area = 3*3 #area del cuadrado formula l*l
+print("El área del cuadrado es:",area,"mts cuadrados",'\n'+"y el perimetro es:",perimetro,"mts")
+#'\n' -> salto de linea (necesario comillas) y usamos "+" para concatenar a la cadena 
