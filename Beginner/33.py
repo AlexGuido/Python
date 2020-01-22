96ef2a9473474505fece4270ddfdfc6ec25bacc0
Alex Guido <34007499+AlexGuido@users.noreply.github.com>
2020-01-22 01:14:39 -0600

    Create 33.py

---
+++ Beginner/33.py
@@ -0,0 +1,10 @@
+'''
+Diseña un programa que, a partir del valor del lado de la base y de la altura de un
+triangulo (3 y 5 metros, respectivame"nte), muestre el valor de su área (en metros cuadrados).
+Recuerda que el área A de un triángulo se puede calcular a partir de la base b y la altura
+h como A = 1/2 bh.
+'''
+altura = 5
+base = 3
+area = 0.5*base*altura
+print("El área del triangulo es:", area,"mts cuadrados")
