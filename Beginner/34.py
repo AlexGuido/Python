82ef2ef3529d05081ed48120006f850f32fc7de5
Alex Guido <34007499+AlexGuido@users.noreply.github.com>
2020-01-22 01:14:56 -0600

    Create 34.py

---
+++ Beginner/34.py
@@ -0,0 +1,11 @@
+'''
+Diseña un programa que, a partir del valor de los dos lados de un rectángulo (4 y 6
+metros, respectivamente), muestre el valor de su perímetro (en metros) y el de su área (en metros
+cuadrados).
+(El perímetro debe darte 20 metros y el área 24 metros cuadrados).
+'''
+base = 4
+altura = 6
+perimetro = 2*base + 2* altura
+area = base * altura
+print("El área del rectangulo es:",area, "mts cuadrados", '\n' + "El perímetro es:", perimetro,"mts")
