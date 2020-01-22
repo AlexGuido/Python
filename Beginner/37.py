28a4d37c929eb5093e3330dd9e32c1a9f8da2ab9
Alex Guido <34007499+AlexGuido@users.noreply.github.com>
2020-01-22 01:15:54 -0600

    Create 37.py

---
+++ Beginner/37.py
@@ -0,0 +1,10 @@
+'''
+Diseña un programa que pida el valor de la base y la altura de un triangulo y muestre el valor de su área. (
+Prueba que tu programa funciona correctamente con este ejemplo: si la base es 10  y la altura 100, el área
+será 500.0)
+'''
+base=float(input("ingresa el valor de la base del triangulo: "))
+altura= float(input("Ingresa el valor de la altura del triangulo: "))
+
+area= (base*altura)/2
+print("El área es: ",area)
