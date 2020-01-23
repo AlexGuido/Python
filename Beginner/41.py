d70b1fe90bfff62ed4abeac925678e9667864594
Alex Guido <34007499+AlexGuido@users.noreply.github.com>
2020-01-23 00:01:11 -0600

    Create 41.py

---
+++ Beginner/41.py
@@ -0,0 +1,6 @@
+'''
+Haz un programa que pida el nombre de una persona y lo muestre en pantalla repetido 1,000 veces, pero dejando un espacio 
+de separación entre aparición y aparición del nombre. (Utiliza los operadores de concatenación y repetición)
+'''
+nombre=input("Ingresa tu nombre: ")
+print((nombre+'\n'+'\n')*1000)
